50bb589db74bc9d647f2e1086cd20ac242fbbc58
karimscoding <rejaulkarim66666@gmail.com>
2024-02-15 00:38:19 +0800

    config: project setup complete

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-cyan-500">
+        Ph University Management
+      </h1>
+    </>
+  );
+}
+
+export default App;
