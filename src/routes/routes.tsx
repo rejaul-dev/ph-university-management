4a6f68ddb3c1c58f9ef87c47baad9be25b2af2c8
karimscoding <rejaulkarim66666@gmail.com>
2024-02-17 03:56:42 +0800

    ci: initialize redux

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import { adminPaths } from "./admin.paths";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <App />,
+    children: adminPaths,
   },
 ]);
 
---
+++ test.js
@@ -0,0 +1,20 @@
+export const adminPaths = [
+  {
+    path: "dashboard",
+    element: "ADMIN_DASHBOARD",
+  },
+  {
+    path: "create-student",
+    element: "CREATE_STUDENT",
+  },
+  {
+    path: "create-admin",
+    element: "CREATE_ADMIN",
+  },
+  {
+    path: "create-faculty",
+    element: "CREATE_FACULTY",
+  },
+];
+
+
